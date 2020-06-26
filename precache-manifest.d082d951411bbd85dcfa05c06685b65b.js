6ffeaeaba7e381daa6b91f7a334766519b83ad91
Chuck Choi <hyunchoi94@gmail.com>
2020-06-26 12:02:22 -0700

    Updates

---
+++ precache-manifest.d082d951411bbd85dcfa05c06685b65b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9576a25be6781392fed4d6268fdf1bb",
+    "url": "/react-lol-champ-picker/index.html"
+  },
+  {
+    "revision": "9ccc9c9443a5852c8046",
+    "url": "/react-lol-champ-picker/static/css/main.53d6be6e.chunk.css"
+  },
+  {
+    "revision": "8961004a6b589eb17a4c",
+    "url": "/react-lol-champ-picker/static/js/2.e1a0f1bd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-lol-champ-picker/static/js/2.e1a0f1bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ccc9c9443a5852c8046",
+    "url": "/react-lol-champ-picker/static/js/main.e47c0f52.chunk.js"
+  },
+  {
+    "revision": "3ee935b36eafbc242477",
+    "url": "/react-lol-champ-picker/static/js/runtime-main.0a68e3c2.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/react-lol-champ-picker/precache-manifest.d082d951411bbd85dcfa05c06685b65b.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-lol-champ-picker/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
